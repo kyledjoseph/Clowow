e800fdee94c4e7e638fdde8c4d30ab68d11ca838
Christian Bundy <me@christianbundy.com>
2013-12-09 17:04:31 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clowow::Application.config.secret_key_base = '927cc250cd4ceb812effa6affb72cb288db6ec33470876b5e5487f426c27dbce862faf887a9b44291489310ade29d8fa3268c6275acd049b91339aebc300054b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Clowow::Application.config.session_store :cookie_store, key: '_clowow_session'

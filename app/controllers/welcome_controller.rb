023090d648e4f893719b4d2956b13eba86e70b5e
Christian Bundy <me@christianbundy.com>
2013-12-11 17:43:05 -0600

    Show comments on feed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,11 @@ class WelcomeController < ApplicationController
 	def index
 		@posts  = Post.all
 		@users = User.all
-		@likes = []
-		@comments = []
+		@comments = [[]]
+		
+		@comment_count = []
+		@like_count = []
+
 		@posts.each do |post|
 			like_count = Like.where(:post_id => post.id).count
 			like_count == 1 ? phrase = 'person likes' : phrase = 'people like'
@@ -11,9 +14,13 @@ class WelcomeController < ApplicationController
 			comment_count = Comment.where(:post_id => post.id).count
 			comment_count == 1 ? word = 'comment' : word = 'comments'
 
-			@likes[post[:id]] = "#{like_count} #{phrase} this"
-			@comments[post[:id]] = "#{comment_count} #{word}"
-
+			@like_count[post[:id]] = "#{like_count} #{phrase} this"
+			@comment_count[post[:id]] = "#{comment_count} #{word}"
+			comment_array = []
+			Comment.where(:post_id =>post.id).each do |comment|
+				comment_array.push(comment.content)
+			end
+			@comments[post[:id]] = comment_array
 		end
 	end
 end

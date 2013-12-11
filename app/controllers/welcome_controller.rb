681e718e9203f7d42249cb2589731d0841306833
Christian Bundy <me@christianbundy.com>
2013-12-11 16:37:52 -0600

    Fix post bug and add comment reading

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,13 +3,21 @@ class WelcomeController < ApplicationController
 		@posts  = Post.all
 		@users = User.all
 		@likes = [[]]
+		@comments = [[]]
 		@posts.each do |post|
 			like_count = Like.where(:post_id => post.id).count
+			comment_count = Comment.where(:post_id => post.id).count
 			if like_count == 1
 				phrase = 'person likes'
 			else
 				phrase = 'people like'
 			end
+			if comment_count == 1
+				word = 'comment'
+			else
+				word = 'comments'
+			end
+			@comments[post[:id]] = "#{comment_count} #{word}"
 			@likes[post[:id]] = "#{like_count} #{phrase} this"
 		end
 	end

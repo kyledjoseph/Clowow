5a533fc7467d1ac6cba41d5bc3c52c52fda9aeb6
Christian Bundy <me@christianbundy.com>
2013-12-11 16:50:59 -0600

    Prettify welcome controller and update view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,23 +2,18 @@ class WelcomeController < ApplicationController
 	def index
 		@posts  = Post.all
 		@users = User.all
-		@likes = [[]]
-		@comments = [[]]
+		@likes = []
+		@comments = []
 		@posts.each do |post|
 			like_count = Like.where(:post_id => post.id).count
+			like_count == 1 ? phrase = 'person likes' : phrase = 'people like'
+			
 			comment_count = Comment.where(:post_id => post.id).count
-			if like_count == 1
-				phrase = 'person likes'
-			else
-				phrase = 'people like'
-			end
-			if comment_count == 1
-				word = 'comment'
-			else
-				word = 'comments'
-			end
-			@comments[post[:id]] = "#{comment_count} #{word}"
+			comment_count == 1 ? word = 'comment' : word = 'comments'
+
 			@likes[post[:id]] = "#{like_count} #{phrase} this"
+			@comments[post[:id]] = "#{comment_count} #{word}"
+
 		end
 	end
 end

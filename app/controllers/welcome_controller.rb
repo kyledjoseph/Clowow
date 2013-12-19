147ffc0b9afaa068e656db702ec26083e1bf9bbf
Alexander Skiba <skiba-alex@ya.ru>
2013-12-19 18:29:14 +0400

    Minor refinements

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,10 @@
 class WelcomeController < ApplicationController
   def index
     @posts = Post.recent.includes(:comments)
-   	@new, @top = 'active', ''
   end
 
   def top
     @posts = Post.most_liked.recent.includes(:comments)
-    @new, @top = '', 'active'
     render "index"
   end
-
-
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,5 @@
 module WelcomeHelper
+  def menu_button(text, url)
+    button_tag text, type: "button", class: "btn btn-default #{'active' if current_page?(url)}", data: { nav: url }
+  end
 end
